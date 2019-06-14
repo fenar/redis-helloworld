3aa12146f5ffa6d4101453c8dcdb3f21ad796781
fatih nar <fenar@yahoo.com>
2019-06-14 23:18:47 +0000

    adding redis osserver creation

---
+++ install-redis-osserver.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# This package is install of opensource redis server
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+apt-get update
+apt-get upgrade -y
+apt-get install -y redis-server
+systemctl enable redis-server.service
+sed -i 's/# 127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+service redis-server restart
+
