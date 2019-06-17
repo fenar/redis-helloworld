df797d8294a42370a7a411e28d021d09d732c5f5
Fatih Nar <fenar@yahoo.com>
2019-06-17 11:32:29 -0500

    Update install-redis-osserver.sh

--- install-redis-osserver.sh
+++ install-redis-osserver.sh
@@ -8,6 +8,6 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y redis-server
 systemctl enable redis-server.service
-sed -i "s#127.0.0.1#0.0.0.0/#" /etc/redis/redis.conf
+sed -i "s#127.0.0.1#0.0.0.0#" /etc/redis/redis.conf
 systemctl restart redis-server
 
