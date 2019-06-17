ab81db40c6f959f5d0db615e329ed02114b11480
fatih nar <fenar@yahoo.com>
2019-06-17 19:07:53 +0000

    minor updates

--- install-redise-replica.sh
+++ install-redise-replica.sh
@@ -4,8 +4,12 @@ if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
-sed -i "s/#DNSStubListener=no/DNSStubListener=yes/g" /etc/systemd/resolved.conf
+apt-get update
+apt-get upgrade -y
+echo 'DNSStubListener=no' >> /etc/systemd/resolved.conf
+mv /etc/resolv.conf /etc/resolv.conf.orig
 ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 service systemd-resolved restart
 tar vxf redislabs-5.4.4-7-bionic-amd64.tar
+mkdir /var/opt/redislabs/log
 ./install.sh -y
